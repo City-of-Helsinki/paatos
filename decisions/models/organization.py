de2a0547405aacfa03e3cd80f2df3f97f1ee2004
Elias Benkhodja <elias.benkhodja@gmail.com>
2017-07-20 11:17:37 +0300

    Fix tests broken by OrganizationClass

--- decisions/models/organization.py
+++ decisions/models/organization.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from .base import DataModel
 
 
-class OrganizationClass(DataModel):
+class OrganizationClass(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     name_fi = models.CharField(max_length=255, null=True)
     name_sv = models.CharField(max_length=255, null=True)
