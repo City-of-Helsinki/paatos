353981434c0f933947438c603ed165ac31a9d9b4
Rikuoja <riku.oja@kapsi.fi>
2018-01-10 17:33:56 +0200

    Add PostClassSerializer to fix post endpoint

--- decisions/api/post.py
+++ decisions/api/post.py
@@ -2,7 +2,7 @@ from django_filters import CharFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import serializers, viewsets
 
-from decisions.models import Action, Post
+from decisions.models import Action, Post, PostClass
 
 from .base import BaseFilter, DataModelSerializer
 
@@ -15,8 +15,15 @@ class PostFilter(BaseFilter):
         fields = BaseFilter.Meta.fields + ('organization',)
 
 
+class PostClassSerializer(DataModelSerializer):
+    class Meta:
+        model = PostClass
+        exclude = ('url',)
+
+
 class PostSerializer(DataModelSerializer):
     actions = serializers.HyperlinkedRelatedField(queryset=Action.objects.all(), many=True, view_name='action-detail')
+    classification = PostClassSerializer()
 
     class Meta:
         model = Post
